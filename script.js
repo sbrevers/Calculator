00228d0c5bac4d49f83a1a90c7efc1b0f9cd17de
sbrevers <bsp001@hotmail.com>
2022-10-12 16:01:48 +0200

    Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const equal = document.querySelector(".equal");
+equal.addEventListener('click', ()=>{
+    console.log(equal);
+})
+
+const buttons = document.querySelectorAll("button");
+button.addEventListener('click', ()=>{
+    consol.log(button);
+})
