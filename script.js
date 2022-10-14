a2a8dbb8f7feec31718c044f3fc9f195a2aba34e
sbrevers <bsp001@hotmail.com>
2022-10-14 12:10:44 +0200

    Test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const equal = document.querySelector(".equal");
+/*const equal = document.querySelector(".equal");
 equal.addEventListener('click', ()=>{
     console.log(equal);
 })
@@ -6,4 +6,27 @@ equal.addEventListener('click', ()=>{
 const buttons = document.querySelectorAll("button");
 button.addEventListener('click', ()=>{
     consol.log(button);
-})
+})*/
+
+const display = document.getElementById('display');
+
+const buttons = Array.from(document.querySelectorAll('button'));
+
+buttons.map (button => button.addEventListener('click', (e) => {
+    switch (e.target.innertext) {
+        case 'c':
+            display.innerText = '';
+            break;
+        case '=':
+            try {
+                display.innerText = eval(display.innerText);
+            }
+            catch {
+                display.innertext = 'Error'
+            }
+            break;
+        default:
+            display.innerText += e.target.innerText;
+            break;
+    }
+}))
